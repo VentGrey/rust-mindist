939f776869732ac953414e9f64d8e1e718bb7e73
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-11-23 01:01:17 -0600

    Modeled things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+struct Iris {
+    num: [f64; 4],
+    id: [char; 20]
+}
+
+struct Comp {
+    num: [f64; 6],
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Abrir el archivo
+    let mut f = BufReader::new(File::open("iris.data").unwrap());
 }
